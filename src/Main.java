9d81523f2e3289f3c0e2d19c4e386cfd4008e8ab
SkykaSmertnay <g925730@gmail.com>
2025-04-07 10:25:57 +0300

    Added all exercises

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,11 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args)
     {
-        //Задание № 1
+        System.out.println("Задание № 1 ");
         int total = 0;
         int[] spent = new int [5] ;
-
         //Scanner scan = new Scanner(System.in);
         Random random = new Random();
-
         for (int i = 0; i < spent.length  ; i++)
         {
             spent[i] = random.nextInt(100, 100000);
@@ -21,24 +19,24 @@ public class Main {
             total = total + i;
         }
         System.out.println("Сумма трат за месяц составила " + total + " рублей.");
-
-        //Задание № 2
+        System.out.println("Задание № 2 ");
         int min = spent[1], max = 0;
         for (int i: spent)
         {
-            if (min > i)
+            if (min > i) {
                 min = i;
-
-            if (max < i)
+            }
+            if (max < i){
                 max = i;
+            }
         }
         System.out.println("Максимальная сумма трат за неделю составила " + max + " рублей.");
         System.out.println("Минимальная сумма трат за неделю составила " + min + " рублей.");
-        //Задание № 3
+        System.out.println("Задание № 3 ");
         float avarege = (float) (total / 4);
 
         System.out.println("Средняя сумма трат за месяц составила " + avarege + " рублей.");
-        //Задание № 4
+        System.out.println("Задание № 4 ");
         int spaceInd = 0;
         char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
         // char[] reverseFullName = { 'a','t', 'e', 'v', 'S', ' ', 'a','v', 'o', 'n', 'a', 'v', 'I'};
@@ -61,7 +59,6 @@ public class Main {
                 reverseFullName[i] = reverseFullName[spaceInd - counter1];
                 reverseFullName[spaceInd - counter1] = save;
                 counter1++;
-
             }
             else if (spaceInd < i && i < (length - 1))
             {
@@ -70,13 +67,10 @@ public class Main {
                 reverseFullName[length-1] = save;
                 length--;
             }
-
-
         }
         for (char c : reverseFullName)
         {
             System.out.print(c);
         }
-
     }
 }
